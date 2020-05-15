8cf49bee2d195b678cc0d4f2be103fa54a8d4295
mistunori-morita <mitsunori.morita@speee.jp>
2020-05-16 01:16:11 +0900

    typsecript array

---
+++ src/array.ts
@@ -0,0 +1,15 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+// 非推奨
+let numbers2: Array<number> = [1, 2, 3];
+let string2: Array<string> = ["Tolyo", "Osaka", "Kyoto"];
+
+let strings: string[] = ["Typescript", "javascript", "React"];
+
+let nijigen: number[][] = [
+  [50, 100],
+  [60, 200],
+];
+
+let hairetu: (string | number | boolean)[] = [1, false, "japan"];
